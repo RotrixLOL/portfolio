e1317cb80dfe11a9f2f6dde5b606d80fd91059cf
RotrixxLOL <ncusnir43@gmail.com>
2023-02-19 17:33:18 +0100

    fix: change logCurrentPage function

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,24 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-import { Navbar } from './components';
-import { logCurrentPage } from './utils/analytics';
-import './App.scss';
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import { logCurrentPage } from "./utils/analytics";
+import "./App.scss";
 
-useEffect(() => {
-  logCurrentPage();
-}, []);
+const App = () => {
+	useEffect(() => {
+		logCurrentPage();
+	}, []);
 
-const App = () => (
-  <div className="app">
-    <Navbar />
-    <Header />
-    <About />
-    <Work />
-    <Skills />
-    <Testimonial />
-    <Footer />
-  </div>
-);
+	return (
+		<div className="app">
+			<Navbar />
+			<Header />
+			<About />
+			<Work />
+			<Skills />
+			<Testimonial />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
--- frontend_react/src/utils/analytics.js
+++ frontend_react/src/utils/analytics.js
@@ -1,27 +1,27 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.GA_API_KEY,
-  authDomain: 'rotrixx-eu.firebaseapp.com',
-  projectId: 'rotrixx-eu',
-  storageBucket: 'rotrixx-eu.appspot.com',
-  messagingSenderId: import.meta.env.GA_MESSAGING_SENDER_ID,
-  appId: import.meta.env.GA_APP_ID,
-  measurementId: import.meta.env.GA_MEASUREMENT_ID,
+	apiKey: "AIzaSyDhh0IJ5oFYNonE2OG-QLZegp3IBPxYYcA",
+	authDomain: "rotrixx-eu.firebaseapp.com",
+	projectId: "rotrixx-eu",
+	storageBucket: "rotrixx-eu.appspot.com",
+	messagingSenderId: "764941317591",
+	appId: "1:764941317591:web:9903afc44b589a9f864792",
+	measurementId: "G-KGQTMFX98Q",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+const handleHashChange = async () => {
+	const { hash } = document.location;
+
+	logEvent(analytics, "screen_view", { screen_name: hash.substring(1) });
+};
+
 export const logCurrentPage = () => {
-  analytics.setCurrentScreen(window.location.pathname);
-  analytics.logEvent('screen_view');
+	handleHashChange();
+	window.addEventListener("hashchange", handleHashChange);
 };
